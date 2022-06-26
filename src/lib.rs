cf4b6bf5a29c5817fec3b08836c57a24ecb834de
kuqmua <serezhashmakov@outlook.com>
2022-06-26 19:02:01 +0300

    prep

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_derive(SvgComponent)]
+pub fn derive_svg_component(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput =
+        syn::parse(input).expect("derive_svg_component syn::parse(input) failed");
+    let ident = &ast.ident;
+    let gen = quote! {
+        impl SvgComponent for #ident {
+            fn get_html(&self) -> Html {
+                match self {
+
+                }
+            };
+            fn get_class(&self) -> AttrValue {
+                match *self {
+
+                }
+            };
+        }
+    };
+    gen.into()
 }
