be1a8dc50fc17df548f66411d5fbfdfc6cb9af3c
kuqmua <serezhashmakov@outlook.com>
2022-08-12 19:33:22 +0300

    local imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use convert_case::Case;
-use convert_case::Casing;
-
 #[proc_macro_derive(SvgComponent)]
-pub fn derive_svg_component(input: TokenStream) -> TokenStream {
+pub fn derive_svg_component(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    use convert_case::Casing;
     let ast: syn::DeriveInput =
         syn::parse(input).expect("derive_svg_component syn::parse(input) failed");
     let ident = &ast.ident;
@@ -15,10 +11,10 @@ pub fn derive_svg_component(input: TokenStream) -> TokenStream {
         syn::Data::Enum(enum_item) => enum_item.variants.into_iter().map(|v| {
             let variant_ident = v.ident;
             let module = syn::Ident::new(
-                &variant_ident.to_string().to_case(Case::Snake),
+                &variant_ident.to_string().to_case(convert_case::Case::Snake),
                 ident.span(),
             );
-            quote!{
+            quote::quote! {
                 SvgType::#variant_ident(svg_props) => html! {
                     <crate::components::ant_design::svg::#module::#variant_ident
                       height={svg_props.height.clone()}
@@ -36,14 +32,17 @@ pub fn derive_svg_component(input: TokenStream) -> TokenStream {
     let get_class_variants = match ast.data {
         syn::Data::Enum(enum_item) => enum_item.variants.into_iter().map(|v| {
             let variant_ident = v.ident;
-            let class =  &format!("anticon-{}", variant_ident.to_string().to_case(Case::Kebab));
-            quote!{
+            let class = &format!(
+                "anticon-{}",
+                variant_ident.to_string().to_case(convert_case::Case::Kebab)
+            );
+            quote::quote! {
                 SvgType::#variant_ident(_) => AttrValue::Static(#class)
             }
         }),
         _ => panic!("SvgComponent works only on enums"),
     };
-    let gen = quote! {
+    let gen = quote::quote! {
         impl SvgComponent for #ident {
             fn get_html(&self) -> Html {
                 match self {
